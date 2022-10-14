14752c60d78ee39855349d2d083914bb93cf6f16
Ilie1985 <ilie.marian32@yahoo.com>
2022-10-14 14:45:02 +0100

    state set in appjs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React,{useState} from "react"
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./components/Menu";
+import Categories from "./components/Categories";
+import items from "./data";
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
   return (
-    <div className="App">
-    <h1>ok</h1>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu />
+      </section>
+    </main>
   );
 }
 
